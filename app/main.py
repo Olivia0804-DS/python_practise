97c5becc6f974826fb6d722170850d82ab7ff1e5
classifier <classifier-2024@lwchen@gmail.com>
2024-10-06 02:13:44 +0000

    modify .gitignore, app/main.py and requirement.txt

--- app/main.py
+++ app/main.py
@@ -1,3 +1,29 @@
 from src.calculator import Calculator
-cal=Calculator()
-print(cal.add(1,2))
+from flask import Flask,render_template,request
+
+Cal=Calculator()
+app=Flask(__name__)
+
+@app.rount("/",methods=["GET","POST"])
+def index():
+    result=""
+    if request.method=="POST":
+        num1=request.form["num1"]
+        num2=request.form["num2"]
+        operator=request.form["operator"]
+        if operator=="+":
+            result=cal.add((float(num1)),float(num2))
+        elif operator=="-":
+            result=cal.substract((float(num1)),float(num2))
+        elif operator=="*":
+            result=cal.multiply((float(num1)),float(num2))
+        else:
+            result=cal.divide((float(num1)),float(num2))
+    return render_template("index.html",result=result)
+
+@app.rount("/health")
+def health():
+    return "I am healthy"
+
+if __name__=="__main__":
+    app.run(debug=True, host='0.0.0.0',port=5000)
